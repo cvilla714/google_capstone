a64ed1e2f44d2cfe655a5dfff0297b960f8b46c7
Cosmel <cosmelvg@gmail.com>
2022-05-30 09:37:07 -0600

    Updating script files
    - load csv files into their own variables
    - add command to look at the columns for each one

--- script_for_project.R
+++ script_for_project.R
@@ -12,3 +12,25 @@ library(ggplot2)
 
 #get my working directory
 getwd()
+
+
+#=====================
+# STEP 1: COLLECT DATA
+#=====================
+
+# Upload Divvy datasets (csv files)
+q2_2019 <- read_csv("Divvy_Trips_2019_Q2.csv")
+q3_2019 <- read_csv("Divvy_Trips_2019_Q3.csv")
+q4_2019 <- read_csv("Divvy_Trips_2019_Q4.csv")
+q1_2020 <- read_csv("Divvy_Trips_2020_Q1.csv")
+
+
+
+#====================================================
+# STEP 2: WRANGLE DATA AND COMBINE INTO A SINGLE FILE
+#====================================================
+colnames(q2_2019)
+colnames(q3_2019)
+colnames(q4_2019)
+colnames(q1_2020)
+
